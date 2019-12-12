a3f7cc2753de31a1429706f0d51cefed3827e89a
ebry <ibrodex@gmail.com>
2019-12-12 14:10:16 +0100

    Creation of Jupyter notebook for insight

--- data/process_data.py
+++ data/process_data.py
@@ -1,5 +1,8 @@
 import sys
+import os
 import pandas as pd
+from sqlalchemy import create_engine
+
 
 def load_data(messages_filepath, categories_filepath):
     '''
@@ -22,15 +25,59 @@ def load_data(messages_filepath, categories_filepath):
     return df
 
 
-
-
-
 def clean_data(df):
-    pass
+    '''
+       INPUT:
+          The function takes the dataframe as merges from 'load_data' and re-creates a columns from the data
+          while dropping the category column.
+          arg: dataframe
+       OUTPUT:
+           df: dataframe having messages and cateries details
+    '''
+
+    # create a dataframe of the each of the category type
+    categories = df.categories.str.split(';', expand=True)
+
+    # select the first row of the categories in the dataframe
+    row = categories.iloc[0, :]
+
+    # convert the row cells to columns using lambda expression.
+    cols = row.apply(lambda x: x[:-2])
+
+    # bind new columns to the `categories` dataframe.
+    categories.columns = cols
+
+    # convert category values to numbers 0 or 1
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].apply(lambda x: x[-1])
+
+        # convert column from string to numeric
+        categories[column] = pd.to_numeric(categories[column])
+
+    df = df.drop('categories', axis=1)  # drop the original categories column from df
+    df = pd.concat([df, categories], axis=1)
+
+    df = df.drop_duplicates()  # drop the duplicates
+
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    '''
+       INPUT:
+           cleansed dataframe having messages and their belonging categories details
+       OUTPUT:
+           database having Messages table
+       DESCRIPTION:
+           Insert dataframe into sql table<DisasterMessages> in database file to be used as input.
+           This checks if the table exist, it it does, it is dropped and recreated. Data is inserted in batches
+           based on the chunksize.
+    '''
+    table = 'DisasterMessages'
+    engine = create_engine('sqlite:///{}'.format(database_filename))
+
+    df.to_sql(name=table, con=engine, if_exists='replace', chunksize=10, index=False)
 
 
 def main():
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,134 @@
 import sys
+import nltk
+
+import sqlalchemy as create_engine
+import pandas as pd
+import re
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import  CountVectorizer, TfidfTransformer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
+import pickle
+
+nltk.download(['punkt', 'wordnet', 'stopwords'])
 
 
 def load_data(database_filepath):
-    pass
+    '''
+          INPUT:
+              database_filepath (str): messages csv files path
+          OUTPUT:
+              X: actual message columns
+              Y: corresponding category values for the message
+              category_columns : category columns
+          DESCRIPTION:
+                 Reads data from sqlite db initially created and selects the columns for the
+                 training and testing model.
+    '''
+    # connect to the database
+    conn = create_engine('sqlite:///{}'.format(database_filepath))
+    # run a query and assign value to a dataframe
+    df = pd.read_sql('SELECT * FROM DisasterMessages', conn)
+
+    # prepare modeling data
+    X = df['message']
+
+    # for the category values columns
+    Y = df.iloc[:, 4:]
+
+    # identify the category columns
+
+    category_columns = Y.coulumns.values
+
+    return X, Y, category_columns
 
 
 def tokenize(text):
-    pass
+    '''
+        The function is to process the sentence, token the words and lower it.
+        arg: str text
+        return:list
+        '''
+    # normalize case and remove punctuation
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+
+    # tokenize text
+    word_list = word_tokenize(text)
+
+    # remove stop words
+    tokens = [w for w in word_list if w not in stopwords.words("english")]
+
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+    return clean_tokens
 
 
 def build_model():
-    pass
+    '''
+        The function is to build a pipeline and using gridsearch to training model.
+        The pipeline including countVectorizer, TfidfTransformer to process the text and using
+        RandomForestClassifier to fit the dataset
+    '''
+
+    # create ML pipeline
+    pipeline = Pipeline([('vect', CountVectorizer(tokenizer=tokenize)),
+                         ('tfidf', TfidfTransformer()),
+                         ('clf', MultiOutputClassifier(RandomForestClassifier()))])
+
+    # specify parameters for grid search
+    parameters = {
+        'features__text_pipeline__vect__ngram_range': ((1, 1), (1, 2)),
+        'features__text_pipeline__vect__max_df': (0.5, 0.75, 1.0),
+        'features__text_pipeline__vect__max_features': (None, 5000, 10000),
+        'features__text_pipeline__tfidf__use_idf': (True, False),
+        'clf__n_estimators': [50, 100, 200],
+        'clf__min_samples_split': [5, 7, 9]
+    }
+
+    model = GridSearchCV(pipeline, param_grid=parameters)
+
+    return model
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    '''
+        The function is to return the results of prediction on test dataset, including precision socre,
+        f1-score and recall score.
+        args: model, test dataset and category names
+        return: dict - the classification report of category names
+    '''
+
+    y_pred = model.predict(X_test)  # prediction
+    prediction = pd.DataFrame(y_pred.reshape(-1, 36), columns=category_names)  # transform list to dataframe
+    report = dict()
+    for i in category_names:
+        # iterate the category names and add its classification scores to dictionary
+        classification = classification_report(Y_test[i], prediction[i])
+        report[i] = classification
+    return report
 
 
 def save_model(model, model_filepath):
-    pass
+    '''
+       INPUT:
+           model (str): trained model
+           model_filepath (str): pickle file path to save the model
+       OUTPUT:
+       DESCRIPTION:
+               save the model passed as the path given as input
+       '''
+
+    pickle.dump(model, open(model_filepath, "wb"))
 
 
 def main():
